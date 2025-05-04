bd8d2b7992b73ed8e5138ee9ee1453f33b1aaff9
ArnoSahakyan <sahakyan.arno@gmail.com>
2025-05-04 23:17:41 +0400

    tour categories implementation

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './auth.routes.js';
 import destinationRoutes from './destinations.routes.js';
 import tourRoutes from './tour.routes.js';
+import tourCategoryRoutes from "./tourCategory.routes.js";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/auth', authRoutes);
 router.use('/destinations', destinationRoutes);
 router.use('/tours', tourRoutes);
+router.use('/tourCategories', tourCategoryRoutes);
 
 export default router;
